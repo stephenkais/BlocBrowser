67d39bd766b37fdfad6d1bb2df9d5a5df293fbb5
stephenkais <stephenkais@gmail.com>
2016-07-07 12:28:50 -0700

    Assignment 23 Completed

--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -39,7 +39,7 @@
     self.textField.returnKeyType = UIReturnKeyDone;
     self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.placeholder = NSLocalizedString(@"Website URL or Google Search", @"Placeholder text for web browser URL field");
     self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
     self.textField.delegate = self;
     
@@ -111,9 +111,13 @@
     [textField resignFirstResponder];
     
     NSString *URLString = textField.text;
-    
+
     NSURL *URL = [NSURL URLWithString:URLString];
     
+    if ([URLString containsString:(@" ")]){
+        URLString = [URLString stringByReplacingOccurrencesOfString:@" " withString:@"+"];
+        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.google.com/search?q=%@", URLString]];
+    }
     
     if (!URL.scheme) {
         // The user didn't type http: or https:
