54f780a74f35abbf9ecf925ba19c1917e8e99349
stephenkais <stephenkais@gmail.com>
2016-07-07 12:01:14 -0700

    Checkpoint 23 completed

--- BlocBrowser/BlocBrowser/AppDelegate.m
+++ BlocBrowser/BlocBrowser/AppDelegate.m
@@ -8,6 +8,7 @@
 // useless comment
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,6 +19,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
